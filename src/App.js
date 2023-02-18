b0ad330341ef8c3589c455e05d3d05d0425a36d7
Mahdialaya5 <mahdibenalaya566@gmail.com>
2023-02-18 11:53:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useLoadScript} from "@react-google-maps/api"
+import Map from './Map';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {isloaded}= useLoadScript({
+    googleMapsApiKey:process.env.googleMapsApiKey
+  }) 
+
+  if(!isloaded) return <div>loading...</div>
+return <Map/>
 }
+
+
 
 export default App;
---
+++ src/Map.js
@@ -0,0 +1,9 @@
+import {GoogleMap} from "@react-google-maps/api"
+const center={lat:25.454,lng:2.54}
+
+function Map(){
+    <div className="App">
+    <GoogleMap center={center} zoom={15}/>
+  </div>
+  }
+  export default Map ;
